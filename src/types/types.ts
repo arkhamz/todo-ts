edeb6d572278d59f35e0e7ba15269ba19bacc7aa
arkhamz <shile2007@gmail.com>
2022-10-08 21:20:31 +0200

    created interfaces for list item and list

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,20 @@
 
+export interface ListItem {
+    id:number;
+    content: string;
+    completed: boolean;
+    listId: number;
+    createdAt: string;
+    updatedAt:string;
+}
 
+export interface List {
+    id:number;
+    name: string;
+    archived: boolean;
+    items: ListItem[];
+    userId: number;
+    createdAt:string;
+    updatedAt:string;
+}
 
-
-export {}
