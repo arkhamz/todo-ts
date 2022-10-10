4c4b2e7da2c8e8040e214502f9434ef722a027df
arkhamz <shile2007@gmail.com>
2022-10-10 12:37:18 +0200

    added styled components package

--- src/components/ItemsPreview.tsx
+++ src/components/ItemsPreview.tsx
@@ -5,7 +5,8 @@ import { List, ListItem } from "../types/types";
 // a popup? overlay?
 function ItemsPreview({data}:{data: List}){
     return <ul className="todo-list">
-        {data && data.items.map(function(i:ListItem) :ReactNode {
+        {/* Only show the first 3 items for each thing */}
+        {data && data.items.slice(0,3).map(function(i:ListItem) :ReactNode {
 
             return <div key={i.id} className="item">
                 <span>{i.content}</span>
--- src/components/ListPreview.tsx
+++ src/components/ListPreview.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { List } from "../types/types";
 import ItemsPreview from "./ItemsPreview"
+import "./ListPreview.css";
 
 // Define the component's props
 type ListPreviewProps = {
@@ -10,10 +11,11 @@ type ListPreviewProps = {
 
 //renders a list of todolists
 function ListPreview({data} : ListPreviewProps){
-    return <ul className="todo-list">
+    return <ul className="prev-lists">
         {data && data.map(function(list:List) : ReactNode{
-            return <div key={list.id}>
-                <h2>{list.name}</h2>
+            return <div className="prev-list-single" key={list.id}>
+                <h3>{list.name}</h3>
+                <h3>{`- ${new Date(list.createdAt).toDateString()}`}</h3>
                 <ItemsPreview data={list} />
                
             </div>
